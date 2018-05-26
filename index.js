26325e0442b493ff7ba4bcaf91c6db6e0072bd63
bakoe <ba.koe96@gmail.com>
2018-05-26 23:48:48 +0200

    Add PlantSample model

--- index.js
+++ index.js
@@ -13,6 +13,24 @@ const sequelize = new Sequelize('plant-tracking-data', 'root', 'root', {
 
 sequelize.authenticate();
 
+const PlantSample = sequelize.define('plant_sample', {
+    sensorId: { type: Sequelize.INTEGER },
+    timestamp: { type: Sequelize.INTEGER },
+    value: { type: Sequelize.INTEGER },
+    unit: { type: Sequelize.STRING }
+});
+
+// force: true will drop the table if it already exists
+PlantSample.sync({force: true}).then(() => {
+    // Table created
+    return PlantSample.create({
+        sensorId: 0,
+        timestamp: 1527347005,
+        value: 24,
+        unit: "celsius"
+    });
+});
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
