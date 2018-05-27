0d62923e6859d62c1b7343a68559747bf569c4d9
birneamstiel <luckifri@yahoo.de>
2018-05-27 06:28:04 +0200

    Set header to allow CORS.

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ Plant.hasMany(Sample);
 sequelize.sync({force: true}).then(() => {
 });
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/api/samples/list', (req, res) => {
